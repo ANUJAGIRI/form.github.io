c305160fe61df7bfda5938aa809f1af47d536e6a
ANUJAGIRI <117623912+ANUJAGIRI@users.noreply.github.com>
2023-07-03 01:04:02 +0530

    form

---
+++ content.php
@@ -0,0 +1,9 @@
+<?php
+ $name = $_post ['name']
+ $age = $_post ['age']
+ $weight = $_post ['weight']
+ $email = $_post ['email']
+ $health_report = $_post ['health_report']
+ 
+//database connection
+?>
